3101cd15fc2585da6956ecdf47490792faaf1dcd
Sheelam Aravind <102232179+AravindSheelam@users.noreply.github.com>
2023-08-14 17:58:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
-//your code here!
 
+const list = document.getElementById('infi-list');
+
+
+function addListItems() {
+
+  const numItemsToAdd = 2;
+
+
+  const currentCount = list.children.length;
+
+  for (let i = 0; i < numItemsToAdd; i++) {
+    const ItemList = document.createElement('li');
+    ItemList.textContent = `Item ${currentCount + i + 1}`;
+    list.appendChild(listItem);
+  }
+}
+
+
+function checkEndOfList() {
+  const listHeight = list.offsetHeight;
+  const scrollTop = list.scrollTop;
+  const scrollHeight = list.scrollHeight;
+
+  if (scrollTop + listHeight >= scrollHeight) {
+    addListItems();
+  }
+}
+
+
+for (let i = 0; i < 10; i++) {
+  const listItem = document.createElement('li');
+  listItem.textContent =  `Item ${i + 1}`;
+  list.appendChild(listItem);
+}
+
+
+list.addEventListener('scroll', checkEndOfList);
